853f56f173b47642286d0463f0eecdad2accc214
animatinator <animatinator@gmail.com>
2022-07-23 17:29:25 +0100

    Add a utility method to generate a random state
    This will be useful for testing things out.

---
+++ utils.py
@@ -0,0 +1,14 @@
+import colour_mapping
+import constants
+from random import randrange
+import state
+
+
+def create_random_state() -> state.TubeBoard:
+    board = state.TubeBoard(tubes = [])
+    for i in range(constants.NUM_TUBES):
+        new_state = state.TubeState(state=[])
+        for i in range(constants.TUBE_DEPTH):
+            new_state.state.append(randrange(1, len(colour_mapping._COLOURS)))
+        board.tubes.append(new_state)
+    return board
