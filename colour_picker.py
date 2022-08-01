c02c0e345098a9b9ae4de6cdb0b4b346702bebf3
animatinator <animatinator@gmail.com>
2022-08-01 19:03:54 +0100

    Add initial UI for adding and deleting colours
    Plus button adds a new colour, right click will open a context menu that
    allows deleting a colour.

--- colour_picker.py
+++ colour_picker.py
@@ -12,18 +12,32 @@ class ColourPicker(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent, background="pink")
 
+        self._frames_container = tk.Frame(self)
+        self._add_colour_button = tk.Button(self, text="+", command=self._add_colour)
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=0)
+        self.rowconfigure(0, weight=1)
+
+        self._frames_container.grid(row=0, column=0, sticky="nsew")
+        self._add_colour_button.grid(row=0, column=1, sticky="nsew")
+
         self._frames = []
 
-        self._frames.append(tk.Frame(self, background="red"))
-        self._frames.append(tk.Frame(self, background="blue"))
-        self._frames.append(tk.Frame(self, background="green"))
+        self._frames.append(tk.Frame(self._frames_container, background="red"))
+        self._frames.append(tk.Frame(self._frames_container, background="blue"))
+        self._frames.append(tk.Frame(self._frames_container, background="green"))
 
         def create_colour_picker_callback(i):
             return lambda event: self._pick_colour(i, event)
 
+        def create_del_context_callback(i):
+            return lambda event: self._show_delete_context_menu(i, event)
+
         for i, frame in enumerate(self._frames):
             frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
             frame.bind("<Button-1>", create_colour_picker_callback(i))
+            frame.bind("<Button-3>", create_del_context_callback(i))
 
     def _pick_colour(self, i, event):
         _, hex_col = askcolor(color=self._frames[i]["background"])
@@ -33,3 +47,9 @@ class ColourPicker(tk.Frame):
     def _update_colours(self):
         print('Colours have changed.')
         print('TODO: Update the model via the controller.')
+    
+    def _add_colour(self):
+        print('Time to add a new colour.')
+    
+    def _show_delete_context_menu(self, i, event):
+        print(f"Delete colour at position {i}?")
